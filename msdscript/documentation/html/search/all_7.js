2f339bf4e57fefa06f60cd159088cbc68a2df878
aidenpratt <u1458057@umail.utah.edu>
2024-02-07 09:36:19 -0700

    lab 4 updates

--- msdscript/documentation/html/search/all_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['has_5fvariable_0',['has_variable',['../class_num.html#a24a51278e76d4b01b457fb5f521bcc38',1,'Num']]]
-];
--- msdscript/documentation/html/search/functions_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['subst_0',['subst',['../class_num.html#a2bd99bf1de2e84f047a077765c625168',1,'Num']]]
-];
